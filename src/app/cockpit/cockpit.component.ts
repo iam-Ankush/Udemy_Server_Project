fd841a83400d9c5017ae7892f83b78cd985a1f09
Ankush <ankush.tiwari18009@gmail.com>
2023-04-02 17:21:09 +0530

    third commit

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,24 +9,31 @@ export class CockpitComponent implements OnInit {
   
  @Output() serverCreated = new EventEmitter<{serverName : string , serverContent : string}>();
  @Output() bluePrintCreated = new EventEmitter<{serverName : string , serverContent : string}>();
-  newServerName : string = '';
-  newServerContent = '';
+  // newServerName : string = '';  --. beacuse in this case we are using Local refrencing
+  // newServerName : string = ''; 
+  // newServerContent = '';  // in this we are doig it with Data Binding
+
+
+  @ViewChild('serverContentInput')
+  serverContentInput!: ElementRef;
+
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  onAddServer() {
+  onAddServer(nameInput : HTMLInputElement) {
     this.serverCreated.emit({
-      serverName : this.newServerName,
-      serverContent : this.newServerContent
+      serverName : nameInput.value,
+      serverContent : this.serverContentInput.nativeElement.value
     })
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput : HTMLInputElement) {
     this.bluePrintCreated.emit({
-      serverName : this.newServerName,
-      serverContent : this.newServerContent
+      serverName : nameInput.value,
+      serverContent : this.serverContentInput.nativeElement.value
     })
   }
 
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,17 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit , ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit , AfterViewInit {
 
  @Input() element!: {type : string , name : string , content : string};
+ @ViewChild('heading', { static: true })
+  header!: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+
+    console.log('text-content ' ,this.header.nativeElement.textContent)
   }
+
+  ngAfterViewInit(){
+    console.log("ngAfterViewInit is called")
+    console.log('text-content ' ,this.header.nativeElement.textContent)
+
+  }
+
+
 
 }
